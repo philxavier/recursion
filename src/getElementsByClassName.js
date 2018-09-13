3789142a5dad621ab40bf56d370d2aa48d65a73b
Felipe <phil.sousa@gmail.com>
2018-09-13 13:01:29 -0700

    finished getElementsByclassName and StringifyJSON.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+
+  var results = [];
+  function recur(x) {
+    if(x.classList && x.classList.contains(className)) {
+      results.push(x)
+    }
+    if(x.childNodes.length > 0) {
+      for (var i = 0; i <x.childNodes.length; i++) {
+        recur(x.childNodes[i])
+      }
+    }
+  }
+  recur(document.body)
+  return results;
 };
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,46 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+  var result=''
+
+  if(obj === null || obj === undefined){
+   obj = null
+   return '' + obj + '';
+  }
+  if(typeof obj === 'number'){
+   return '' + obj + '';
+  }
+  if(typeof obj === 'boolean'){
+   return '' + obj + '';
+  }
+  if(typeof obj === 'string'){
+   return ('"' + obj + '"');
+  }
+  if(Array.isArray(obj) && typeof obj === 'object') {
+    result+='['
+    for (var i = 0; i < obj.length; i++) {
+     if(i !== obj.length-1) {
+       result += stringifyJSON(obj[i]) +','
+     } else {
+       result += stringifyJSON(obj[i])
+     }
+    }
+    result += ']';
+  } else {
+    result += '{'
+    var keys = Object.keys(obj)
+    var lastKey = keys[keys.length-1]
+    for (var key in obj) {
+      if (typeof obj[key] !== 'function' && obj[key] !== undefined) {
+        if(key !== lastKey) {
+         result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]) +',';
+        } else {
+          result += stringifyJSON(key) + ':' + stringifyJSON(obj[key]);
+        }
+      }
+    }
+    result+='}'
+  }
+  return result;
 };
